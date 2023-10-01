6eae0313b7f23ebca1f70b1dc481b02e66397174
ismael <ismael.castillo@omegasoftve.com>
2023-09-30 20:20:38 -0400

    last changes

--- src/components/onlineRadio/OnlineRadio.jsx
+++ src/components/onlineRadio/OnlineRadio.jsx
@@ -1,10 +1,12 @@
 import { React, useState, useContext, useEffect, useRef } from 'react'
 import { io } from "socket.io-client";
 import axios from "axios";
+import styledComponents from '../styled';
 import { Container, FormGroup, FormControl, InputLabel, Input, Button } from '@mui/material';
 import { styled } from '@mui/system';
 import consts from '../../settings/consts';
 import AppContext from '../../context/App';
+import IconButton from '../../icons/radio-online-icon.svg'
 
 const ContainerComponent = styled('div')({
   padding: '20px'
@@ -14,6 +16,8 @@ const ContainerComponent = styled('div')({
 const OnlineRadio = ({}) => {
   const [ audioBlobURL, setAudioBlobURL ] = useState("");
   //const playElement = useRef();
+  const H1 = styledComponents.radioOnlineh1;
+  const ImgIcon = styledComponents.radioOnlineIcon;
   const socket = io(consts.ws_server_url);
   const sendMessage = () => {
   }
@@ -48,7 +52,12 @@ const OnlineRadio = ({}) => {
   return (
     <ContainerComponent>
       <Container maxWidth="sm">
-        <h1>Reproductor de Radio</h1>
+        <H1>Radio Ecológica</H1>
+        <br />
+        <Container maxWidth="xl" className="bg-teal-400  p-6 rounded-md">
+            <h2>Hola Mundo!!</h2>
+            <ImgIcon src={IconButton} alt="React Logo" className="radio-online-icon"/>
+        </Container>
       </Container>
     </ContainerComponent>
   )
--- src/components/styled.jsx
+++ src/components/styled.jsx
@@ -3,15 +3,21 @@ const styledComponents = {
     dahsboardPanelh1: styled('h1')({
         color: 'blue',
         fontSize: '2rem',
-        fontWeight: '700',
-        color: '#1f2937'
+        fontWeight: '700'
     }),
     dahsboardPanelh2: styled('h2')({
         color: 'blue',
         fontSize: '1.6rem',
-        fontWeight: '700',
-        color: '#1f2937'
+        fontWeight: '700'
     }),
+    radioOnlineh1: styled('h1')({
+        fontSize: '2rem',
+        fontWeight: '700',
+        color: '#4FC0D0'
+    }),
+    radioOnlineIcon: styled('img')({
+        width: '100px !important'
+    })
 };
 
 export default styledComponents;
