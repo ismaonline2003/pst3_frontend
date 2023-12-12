e735f7b82b9f2b49091968948196d9098edd7d89
ismael <ismael.castillo@omegasoftve.com>
2023-12-12 06:30:24 -0400

    last changes

--- src/components/userPanel/grabacionesPanel/GrabacionForm.jsx
+++ src/components/userPanel/grabacionesPanel/GrabacionForm.jsx
@@ -1,8 +1,264 @@
-import { React, useState, useContext, useEffect, Fragment } from 'react'
+import { useState, useEffect, useContext } from 'react';
+import { useParams, Navigate, Link } from 'react-router-dom';
+import axios from "axios";
+import AppContext from '../../../context/App';
+import consts from '../../../settings/consts';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import FormBtns from '../FormBtns';
+import sequelizeImg2Base64 from '../../../helpers/sequelizeImg2Base64';
+import getFormattedDate from '../../../helpers/getFormattedDate';
+import styledComponents from '../../styled'
+import FormContainer from '../FormContainer'
+import noEncontrado from '../../../icons/no-encontrado.jpg'
+import Button from '@mui/material/Button';
+import getTurnoName from '../../../helpers/getTurnoName';
+import DeleteDialog from '../../generales/DeleteDialog';
 
 const GrabacionForm = ({}) => {
+    //logic
+    const [reLoad, setReload] = useState(false);
+    const [redirect, setRedirect] = useState(false);
+    const [showFormBtns, setShowFormBtns] = useState(true);
+    const [recordData, setRecordData] = useState(false);
+
+    //bd values
+    const [nombre, setNombre] = useState("");
+    const [emisor, setEmisor] = useState("");
+    const [descripcion, setDescripcion] = useState("");
+    const [status, setStatus] = useState("");
+    const [duracion, setDuracion] = useState("");
+    const [fechaInicio, setFechaInicio] = useState("");
+    const [fechaFin, setFechaFin] = useState("");
+    const [urlGrabacion, setUrlGrabacion] = useState("");
+
+    //Ui fields
+    const [showDeleteDialog, setShowDeleteDialog ] = useState(false);
+    const [recordFound, setRecordFound] = useState(false);
+    const [newId, setNewId] = useState(0);
+    const { id } = useParams();
+    const { blockUI, setBlockUI, setNotificationMsg, setNotificationType, setShowNotification} = useContext(AppContext);
+    const [unlockFields, setUnlockFields] = useState(false);
+    const StyledH1 = styledComponents.dahsboardPanelh1;
+    const StyledH2 = styledComponents.dahsboardPanelh2;
+
+    const _setRecordData = (data) => {
+        let duracionMinutos = data.duracion/60;
+        if(duracionMinutos != 1) {
+            duracionMinutos = `${duracionMinutos} minutos`
+        } else {
+            duracionMinutos = `${duracionMinutos} minuto`
+        }
+        setRecordData(data);
+        setNombre(data.titulo);
+        setEmisor(`${data.user.person.name} ${data.user.person.lastname}`);
+        setDescripcion(data.descripcion);
+        setStatus("Finalizada");
+        setDuracion(duracionMinutos);
+        setFechaInicio(getFormattedDate(new Date(data.fecha_inicio)));
+        setFechaFin(getFormattedDate(new Date(data.fecha_fin)));
+        setUrlGrabacion(`${consts.backend_base_url}/reproducirGrabacion/${data.id}`);
+    }
+
+    const recordValidations = () =>  {
+        let objReturn = {'status': 'success', 'data': {}, 'msg': ''};
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        if(nombre.trim() == "") {
+            objReturn = {'status': 'failed', 'data': {}, 'msg': 'Se debe definir un nombre para la sección.'};
+            return objReturn;
+        }
+        setNombre(nombre.trim());
+
+        return objReturn;
+    }
+
+    const searchRecord = () => {
+        setBlockUI(true);
+        if(id != "0") {
+            const token = localStorage.getItem('token');
+            const config = {headers:{ authorization: token}};
+            let url = `${consts.backend_base_url}/api/grabacion/${id}`;
+            axios.get(url, config).then((response) => {
+                //set record data
+                _setRecordData(response.data);
+                setBlockUI(false);
+            }).catch((err) => {
+                if(err.response.status == 404) {
+                    setRecordFound(false);
+                } else {
+                    setNotificationMsg("Ocurrió un error inesperado... Intentelo mas tarde.");
+                    setNotificationType('error');
+                    setShowNotification(true);
+                }
+                setBlockUI(false);
+            });
+        } else {
+            setRecordFound(true);
+            setUnlockFields(true);
+            setBlockUI(false);
+        }
+    }
+
+    const updateRecordData = () => {
+        setBlockUI(true);
+        const token = localStorage.getItem('token');
+        let body = {
+            nombre: nombre
+        };
+        const config = {headers:{'authorization': token}};
+        let url = `${consts.backend_base_url}/api/grabacion/${id}`;
+        axios.put(url, body, config).then((response) => {
+            //set record data
+            _setRecordData(response.data);
+            setBlockUI(false);
+            setNotificationMsg(response.data.message);
+            setNotificationType('success');
+            setShowNotification(true);
+        }).catch((err) => {
+            setNotificationMsg(err.response.data.message);
+            setNotificationType('error');
+            setShowNotification(true);
+            setBlockUI(false);
+        });
+    }
+
+    const createRecord = () => {
+        setBlockUI(true);
+        const token = localStorage.getItem('token');
+        let body = {
+            nombre: nombre
+        };
+        const config = {headers:{'authorization': token}};
+        let url = `${consts.backend_base_url}/api/grabacion`;
+        axios.post(url, body, config).then((response) => {
+            //set record data
+            _setRecordData(response.data);
+            setNotificationMsg("El registro fue creado exitosamente!!");
+            setNotificationType('success');
+            setShowNotification(true);
+            setReload(true);
+            setBlockUI(false);
+        }).catch((err) => {
+            console.log(err);
+            setNotificationMsg(err.response.data.message);
+            setNotificationType('error');
+            setShowNotification(true);
+            setBlockUI(false);
+        });
+    }
+
+    const handleConfirmarBtn = (e) => {
+        let confirmarBtnReturn = {'status': 'success', 'message': '', 'data': {}};
+        confirmarBtnReturn = recordValidations();
+        if(confirmarBtnReturn.status == 'success') {
+            if(recordData != false) {
+                updateRecordData();
+            } else {
+                createRecord();
+            }
+        }
+        return confirmarBtnReturn;
+    }
+
+    const handleCancelarBtn = (e) => {
+        if(id != '0') {
+            _setRecordData(recordData);
+        } else {
+            setRedirect(true);
+        }
+    }
+
+    const handleDeleteBtn = () => {
+        setShowDeleteDialog(true);
+    }
+
+    const handleDeleteConfirm = (e) => {
+        setBlockUI(true);
+        const token = localStorage.getItem('token');
+        const config = {headers:{'authorization': token}};
+        let url = `${consts.backend_base_url}/api/grabacion/${id}`;
+        axios.delete(url, config).then((response) => {
+            setBlockUI(false);
+            setNotificationMsg(response.data.message);
+            setNotificationType('success');
+            setShowNotification(true);
+            setShowFormBtns(false);
+            setTimeout(() => {
+                setRedirect(true);
+            }, 5000);
+        }).catch((err) => {
+            setNotificationMsg(err.response.data.message);
+            setNotificationType('error');
+            setShowNotification(true);
+            setBlockUI(false);
+        });
+    }
+
+    useEffect(() => {
+        searchRecord();
+    }, []);
+
     return (
-        <div></div>
-    );
-};
+        <div className='m-4'>
+            {
+                <div className='text-center mb-10'>
+                    <StyledH1>Grabación</StyledH1>
+                </div>
+            }
+            {
+                reLoad && <Navigate to={`/dashboard/grabaciones/${newId}`} />
+            }
+            {
+                redirect && <Navigate to="/dashboard/grabaciones" />
+            }
+            {
+                <FormContainer>
+                    <div className='d-flex flex-row flex-wrap'>
+
+                    </div>
+                    <div className='d-flex flex-row flex-wrap'>
+                        <FormControl sx={{ m: 1, width: '95%' }} variant="outlined">
+                            <TextField id="nombre" label="Título" disabled variant="outlined" value={nombre}/>
+                        </FormControl>
+                    </div>
+                    <div className='d-flex flex-row flex-wrap'>
+                        <FormControl sx={{ m: 1, width: '31%' }} variant="outlined">
+                            <TextField id="Emisor" label="Emisor" disabled variant="outlined" value={emisor}/>
+                        </FormControl>
+                        <FormControl sx={{ m: 1, width: '31%' }} variant="outlined">
+                            <TextField id="status" label="Estatus" disabled variant="outlined" value={status}/>
+                        </FormControl>
+                        <FormControl sx={{ m: 1, width: '31%' }} variant="outlined">
+                            <TextField id="duracion" label="Duracion" disabled variant="outlined" value={duracion}/>
+                        </FormControl>
+                    </div>
+                    <div className='d-flex flex-row flex-wrap'>
+                        <FormControl sx={{ m: 1, width: '95%' }} variant="outlined">
+                            <TextField id="descripcion" label="Descripcion" disabled variant="outlined" value={descripcion}/>
+                        </FormControl>
+                    </div>
+                    <div className='d-flex flex-row flex-wrap'>
+                        <FormControl sx={{ m: 1, width: '47%' }} variant="outlined">
+                            <TextField id="inicio" label="Inicio" disabled variant="outlined" value={fechaInicio}/>
+                        </FormControl>
+                        <FormControl sx={{ m: 1, width: '47%' }} variant="outlined">
+                            <TextField id="fin" label="Fin" disabled variant="outlined" value={fechaFin}/>
+                        </FormControl>
+                    </div>
+                    <div className='d-flex flex-row flex-wrap m-4'>
+                        <a href={urlGrabacion} target='_blank'>Reproducir la Grabación</a>
+                    </div>
+                </FormContainer>
+            }
+            {
+                showDeleteDialog &&
+                <DeleteDialog showDeleteDialog={showDeleteDialog} setShowDeleteDialog={setShowDeleteDialog} handleDeleteConfirm={handleDeleteConfirm}></DeleteDialog>
+            }
+
+        </div>
+    )
+}
+
 export default GrabacionForm;
--- src/components/userPanel/grabacionesPanel/Grabaciones.jsx
+++ src/components/userPanel/grabacionesPanel/Grabaciones.jsx
@@ -1,10 +1,140 @@
-import { React, useState, useContext, useEffect, Fragment } from 'react'
+import * as React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import axios from "axios";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import SearchBarReadOnly from '../SearchBarReadOnly';
+import styledComponents from '../../styled';
+import AppContext from '../../../context/App';
+import consts from '../../../settings/consts';
+import getTurnoName from '../../../helpers/getTurnoName';
+//simple table
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+//link
+import { Link } from 'react-router-dom';
 
-const Grabaciones = ({}) => {
+const searchBarParameters = [
+    { value: 'ref', label: 'Referencia', type: "number"},
+    { value: 'nombre', label: 'Nombre', type: "search"},
+    { value: 'emisor', label: 'Emisor', type: "search"},
+    { value: 'fecha', label: 'Fecha', type: "search"}
+];
+const searchSelectValues = [];
+
+export default function Grabaciones({}) {
+    const StyledH1 = styledComponents.dahsboardPanelh1;
+    const [ records, setRecords ] = useState([]);
+    const { blockUI, setBlockUI, setNotificationMsg, setNotificationType, setShowNotification} = useContext(AppContext);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+
+    const searchRecords = (searchVals) => {
+        setBlockUI(true);
+        const token = localStorage.getItem('token');
+        const config = {headers:{ authorization: token}};
+        let url = `${consts.backend_base_url}/api/grabacion?limit=${searchVals.limit}`;
+        if(searchVals.parameter != "" && searchVals.value != "") {
+            url += `&parameter=${searchVals.parameter}&value=${searchVals.value}`;
+        }
+        axios.get(url, config).then((response) => {
+            console.log(response.data);
+            setRecords(response.data);
+            setBlockUI(false);
+        }).catch((err) => {
+          setNotificationMsg("Ocurrió un error inesperado... Intentelo mas tarde.");
+          setNotificationType('error');
+          setShowNotification(true);
+          setBlockUI(false);
+        });
+    }
+    useEffect(() => {
+        searchRecords({
+            parameter: "",
+            value: "",
+            limit: 25,
+        });
+    }, []);
+
+    const handleSearchBtn = (searchVals) => {
+        searchRecords(searchVals);
+    };
+
     return (
-        <div>
-            <h1>Hola Mundo</h1>
-        </div>
+        <React.Fragment>
+            <br /> 
+            <StyledH1>Grabaciones</StyledH1>
+            <br />
+            <SearchBarReadOnly selectOptions={searchBarParameters} externalHandleSearchBtn={handleSearchBtn} crearRoute={"/dashboard/grabaciones/0"} searchSelectValues={searchSelectValues}/>
+            <br />
+            <div style={{ height: 400, width: '100%' }}>
+            <Paper sx={{ width: '100%', mb: 2 }}>
+                <TableContainer>
+                    <Table sx={{ minWidth: 650 }} aria-label="Proyectos">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="left">Ref</TableCell>
+                                <TableCell align="left">Fecha</TableCell>
+                                <TableCell align="left">Nombre</TableCell>
+                                <TableCell align="left">Emisor</TableCell>
+                                <TableCell align="left">Duración</TableCell>
+                                <TableCell align="left">Ver</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {console.log(records)}
+                        {records.filter((item, index) => index < (page+1)*rowsPerPage && index >= (page)*rowsPerPage).map((row, index) => {
+                                const recordDate = new Date(row.fecha_inicio);
+                                return (
+                                    <TableRow
+                                        key={row.id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row">
+                                                {row.id}
+                                            </TableCell>
+                                            <TableCell align="left">{recordDate.getDate()}/{recordDate.getMonth()+1}/{recordDate.getFullYear()}</TableCell>
+                                            <TableCell align="left">{row.titulo}</TableCell>
+                                            <TableCell align="left">{row.user.person.name} {row.user.person.lastname}</TableCell>
+                                            <TableCell align="left">{row.duracion}</TableCell>
+                                            <TableCell align="left">
+                                                <Link to={`/dashboard/grabaciones/${row.id}`}>
+                                                    <VisibilityIcon color="secondary"/>
+                                                </Link>
+                                            </TableCell>
+                                    </TableRow>
+                                )
+                        })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={records.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                 />
+            </Paper>
+            </div>
+        </React.Fragment>
     );
-};
-export default Grabaciones;
+}
