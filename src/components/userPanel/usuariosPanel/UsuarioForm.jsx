d41354ef51dbe94b58c83343a1c02abb5e10f2c1
ismael <ismael.castillo@omegasoftve.com>
2023-12-15 01:00:54 -0400

    last changes

--- src/components/userPanel/usuariosPanel/UsuarioForm.jsx
+++ src/components/userPanel/usuariosPanel/UsuarioForm.jsx
@@ -1,23 +1,32 @@
-import { useState, useEffect, useContext } from 'react';
+//react
+import { useState, useEffect, useContext, Fragment } from 'react';
 import { useParams, Navigate, Link } from 'react-router-dom';
 import axios from "axios";
-import AppContext from '../../../context/App';
-import consts from '../../../settings/consts';
+
+//material ui
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
+import Switch from '@mui/material/Switch';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Button from '@mui/material/Button';
+
+//own
+import AppContext from '../../../context/App';
+import consts from '../../../settings/consts';
 import FormBtns from '../FormBtns';
 import sequelizeImg2Base64 from '../../../helpers/sequelizeImg2Base64';
 import getFormattedDate from '../../../helpers/getFormattedDate';
 import styledComponents from '../../styled'
 import FormContainer from '../FormContainer'
 import noEncontrado from '../../../icons/no-encontrado.jpg'
-import Button from '@mui/material/Button';
 import getTurnoName from '../../../helpers/getTurnoName';
 import DeleteDialog from '../../generales/DeleteDialog';
 
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+const changePasswordLabel = { inputProps: { 'aria-label': 'Cambiar Contraseña' } };
+
 
 const UsuarioForm = ({}) => {
     //logic
@@ -38,6 +47,7 @@ const UsuarioForm = ({}) => {
     const [ personaSearchBar, setPersonaSearchBar] = useState("");
     const [ personasList , setPersonasList] = useState([]);
     const [ personaSelected , setPersonaSelected] = useState(false);
+    const [ showPasswordFields, setShowPasswordFields ] = useState(false);
     const [ newPassword, setNewPassword] = useState("");
     const [ showNewPassword, setShowNewPassword] = useState(false);
     const [ newPasswordInputType, setNewPasswordInputType] = useState('password');
@@ -68,18 +78,62 @@ const UsuarioForm = ({}) => {
         const currentDate = new Date();
         const currentYear = currentDate.getFullYear();
         let emailRegExp =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/g;
+        const specialCharactersAllowed = /[!@#$%&*?]/;
+        const specialCharsNotAllowed = /[^()_+\-=\[\]{};':"\\|,.<>\/~]/g;
+
         if(login.trim() == "") {
             objReturn = {'status': 'failed', 'data': {}, 'msg': 'Se debe definir un email valido para el usuario.'};
             return objReturn;
         }
+        
         setLogin(login);
+
         if(!emailRegExp.test(login)) {
-            objReturn = {status: 'error', data: {}, message: 'El correo electrónico especificado es inválido.'};
+            objReturn = {status: 'failed', data: {}, msg: 'El correo electrónico especificado es inválido.'};
             return objReturn;
         }
-        if(!personaSelected) {
-            objReturn = {status: 'error', data: {}, message: 'Debe seleccionar una persona para el usuario.'};
-            return objReturn;
+
+        if(id == '0') {
+            if(!personaSelected) {
+                objReturn = {status: 'failed', data: {}, msg: 'Debe seleccionar una persona para el usuario.'};
+                return objReturn;
+            }
+        }
+        if(id == '0' || showPasswordFields) {
+            if(newPassword.length < 12) {
+                objReturn = {status: 'failed', data: {}, msg: 'La contraseña debe tener un mínimo 12 caracteres.'};
+                return objReturn;
+            }
+    
+            if(/\s/.test(newPassword)) {
+                objReturn = {status: 'failed', data: {}, msg: 'La contraseña no puede tener espacios en blanco.'};
+                return objReturn;
+            }
+    
+            if(!/[A-Z]/.test(newPassword)) {
+                objReturn = {status: 'failed', data: {}, msg: 'La contraseña debe tener al menos una letra mayúscula.'};
+                return objReturn;
+            }
+    
+            if(!/[a-z]/.test(newPassword)) {
+                objReturn = {status: 'failed', data: {}, msg: 'La contraseña debe contener letras minusculas.'};
+                return objReturn;
+            }
+    
+            if(!/\d/.test(newPassword)) {
+                objReturn = {status: 'failed', data: {}, msg: 'La contraseña debe contener al menos un caracter numérico.'};
+                return objReturn;
+            }
+    
+            if(!specialCharactersAllowed.test(newPassword)) {
+                objReturn = {status: 'failed', data: {}, msg: `La contraseña debe contener al menos uno de los siguientes caracteres especiales: !@#$%&*?`};
+                return objReturn;
+            }
+    
+            if(newPassword != newPasswordRepeated) {
+                objReturn = {status: 'failed', data: {}, msg: `Ambas contraseñas deben ser iguales.`};
+                return objReturn;
+            }
         }
         return objReturn;
     }
@@ -91,8 +145,10 @@ const UsuarioForm = ({}) => {
             const config = {headers:{ authorization: token}};
             let url = `${consts.backend_base_url}/api/users/${id}`;
             axios.get(url, config).then((response) => {
+                console.log(response.data);
                 //set record data
                 _setRecordData(response.data);
+                setRecordFound(true);
                 setBlockUI(false);
             }).catch((err) => {
                 if(err.response.status == 404) {
@@ -115,13 +171,17 @@ const UsuarioForm = ({}) => {
         setBlockUI(true);
         const token = localStorage.getItem('token');
         let body = {
-            login: login
+            login: login,
+            change_password: showPasswordFields,
+            password: newPassword,
+            password_repeat: newPasswordRepeated
         };
         const config = {headers:{'authorization': token}};
         let url = `${consts.backend_base_url}/api/users/${id}`;
         axios.put(url, body, config).then((response) => {
             //set record data
-            _setRecordData(response.data);
+            console.log(response);
+            _setRecordData(response.data.data[0]);
             setBlockUI(false);
             setNotificationMsg(response.data.message);
             setNotificationType('success');
@@ -151,10 +211,13 @@ const UsuarioForm = ({}) => {
             setNotificationMsg("El registro fue creado exitosamente!!");
             setNotificationType('success');
             setShowNotification(true);
-            setReload(true);
             setBlockUI(false);
+            setNewId(response.data.id);
+            setTimeout(() => {
+                setReload(true);
+            }, 3000)
         }).catch((err) => {
-            console.log(err);
+            setUnlockFields(true);
             setNotificationMsg(err.response.data.message);
             setNotificationType('error');
             setShowNotification(true);
@@ -169,6 +232,7 @@ const UsuarioForm = ({}) => {
             if(recordData != false) {
                 updateRecordData();
             } else {
+                confirmarBtnReturn.status = 'new_record';
                 createRecord();
             }
         }
@@ -304,6 +368,55 @@ const UsuarioForm = ({}) => {
                             </FormControl>
                         }
                     </div>
+                    <div className='d-flex flex-row flex-wrap'>
+                        <FormControl sx={{ m: 1, width: '95%' }} variant="outlined">
+                            <TextField id="persona_id" label="Datos del Usuario" disabled variant="outlined" value={`${personaData.name} ${personaData.lastname} - C.I: ${personaData.ci_type}-${personaData.ci}`}/>
+                        </FormControl>
+                    </div>
+                    <div className='d-flex flex-row flex-wrap'>
+                        {
+                            unlockFields && 
+                            <Fragment>
+                                <span>¿Desea cambiar la contraseña?</span>
+                                <Switch {...changePasswordLabel} onClick={() => setShowPasswordFields(!showPasswordFields)}/>
+                            </Fragment>
+                        }
+                    </div>
+                    {
+                        showPasswordFields && unlockFields &&
+                        <Fragment>
+                            <div className='d-flex flex-row flex-wrap'>
+                                <FormControl sx={{ m: 1, width: '85%' }} variant="outlined">
+                                    <TextField id="password" label="Escriba la nueva contraseña" type={newPasswordInputType} variant="outlined" defaultValue={""} onChange={(e) => setNewPassword(e.target.value)}/>
+                                </FormControl>
+                                <FormControl sx={{ m: 1, width: '10%' }} variant="outlined">
+                                    {
+                                        showNewPassword && 
+                                        <VisibilityOffIcon style={{fontSize: '2.2rem', marginLeft: '10px', cursor: 'pointer'}} onClick={(e) => _handleShowNewPasswordBtn(!showNewPassword)}/>
+                                    }
+                                    {
+                                        !showNewPassword && 
+                                        <RemoveRedEyeIcon style={{fontSize: '2.2rem', marginLeft: '10px', cursor: 'pointer'}}  onClick={(e) => _handleShowNewPasswordBtn(!showNewPassword)}/>
+                                    }
+                                </FormControl>
+                            </div>
+                            <div className='d-flex flex-row flex-wrap'>
+                                <FormControl sx={{ m: 1, width: '85%' }} variant="outlined">
+                                    <TextField id="password_repeated" label="Repita la nueva contraseña" type={newPasswordRepeatedInputType} variant="outlined" defaultValue={""} onChange={(e) => setNewPasswordRepeated(e.target.value)}/>
+                                </FormControl>
+                                <FormControl sx={{ m: 1, width: '10%' }} variant="outlined">
+                                    {
+                                        showNewPasswordRepeated && 
+                                        <VisibilityOffIcon style={{fontSize: '2.2rem', marginLeft: '10px', cursor: 'pointer'}} onClick={(e) => _handleShowNewPasswordRepeatedBtn(!showNewPasswordRepeated)}/>
+                                    }
+                                    {
+                                        !showNewPasswordRepeated && 
+                                        <RemoveRedEyeIcon style={{fontSize: '2.2em', marginLeft: '10px', cursor: 'pointer'}}  onClick={(e) => _handleShowNewPasswordRepeatedBtn(!showNewPasswordRepeated)}/>
+                                    }
+                                </FormControl>
+                            </div>
+                        </Fragment>
+                    }
                 </FormContainer>
             }
 
@@ -365,7 +478,20 @@ const UsuarioForm = ({}) => {
                 showDeleteDialog &&
                 <DeleteDialog showDeleteDialog={showDeleteDialog} setShowDeleteDialog={setShowDeleteDialog} handleDeleteConfirm={handleDeleteConfirm}></DeleteDialog>
             }
-
+            {
+                (!recordFound && id != 0) && 
+                <FormContainer>
+                    <div className='text-center'>
+                        <img src={noEncontrado} alt="no-encontrado" style={{width: '300px', margin: '0 auto', marginTop:'10px'}}/>
+                        <StyledH2 className='mt-5'>El usuario no fue encontrado</StyledH2>
+                        <div className='text-center d-flex flex-row flex-wrap w-100 mt-5'>
+                            <Link to={"/dashboard/usuarios"}>
+                                <Button variant="contained" color="primary" style={{marginLeft: '10px'}}>Volver</Button>
+                            </Link>
+                        </div>
+                    </div>
+                </FormContainer>
+            }
         </div>
     )
 }
